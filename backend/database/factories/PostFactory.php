bc72f7eb9fe4fd1c7923f578f2fb5a8ca0568395
Nguyen Khac Duy Ngoc <nguyen.khac.duy.ngoc@sun-asterisk.com>
2020-02-11 13:24:11 +0700

    add basic seeder for DB

--- backend/database/factories/PostFactory.php
+++ backend/database/factories/PostFactory.php
@@ -2,11 +2,17 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Post;
+use App\Models\Post;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => $faker->numberBetween(1, 10),
+        'category_id' => $faker->numberBetween(1, 10),
+        'title' => $faker->sentence(9),
+        'subtitle' => $faker->paragraph(4),
+        'content' => $faker->text(),
+        'status' => $faker->numberBetween(1, 3),
+        'avg_rate' => 0
     ];
 });
