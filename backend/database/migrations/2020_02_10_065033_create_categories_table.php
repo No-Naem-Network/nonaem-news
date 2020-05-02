efd94bd22006cbc77cb8cab52732b3783ee83822
Ngọc Nguyễn Khắc Duy <ngoc.nguyenkhacduy@cyberlogitec.com>
2020-05-02 16:58:39 +0700

    fix migrate title category from string to text and move title to top of auth page

--- backend/database/migrations/2020_02_10_065033_create_categories_table.php
+++ backend/database/migrations/2020_02_10_065033_create_categories_table.php
@@ -8,23 +8,19 @@ class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 100);
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
